717f07ef3f194c25a21f03f6d6b88edd2edac100
dbhujle <bhujle.Deepa@gmail.com>
2019-06-01 00:24:53 -0500

    another set of comments

--- server.js
+++ server.js
@@ -8,15 +8,18 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
+//usual routing etc
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname + '/home.html'));
     //res.send("Welcome to Friend Catcher!");
 })
 
+//survey page
 app.get("/survey", function(req, res){
     res.sendFile(path.join(__dirname , "/survey.html"));
 })
 
+//friends list
 app.get("/API/friends", function(req, res){
     res.sendFile(path.join(__dirname , "/friends.html"));
 })
