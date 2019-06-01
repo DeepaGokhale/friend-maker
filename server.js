bb966aeb60f9623cb99d1c0cdf56a00ad6316c20
dbhujle <bhujle.Deepa@gmail.com>
2019-05-31 22:19:50 -0500

    Update # 3

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 var express = require("express");
+var path = require("path");
 var app = express();
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static('public'));
+
 app.get("/", function(req, res){
-    res.send("Welcome to Friend Catcher!");
+    res.sendFile(path.join(__dirname + '/home.html'));
+    //res.send("Welcome to Friend Catcher!");
+})
+
+app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname , "/survey.html"));
+})
+
+app.get("/API/friends", function(req, res){
+    res.sendFile(path.join(__dirname , "/friends.html"));
 })
 
 app.listen(PORT, function(){
